fb0bfa350f848c0577ad003929a557e298f1217d
Tamojit Das <tamojitdas181007@gmail.com>
2024-03-28 20:45:58 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -9,18 +11,23 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser()); //to parse cookies anywhere in the app.
 
-/*app.get("/", (req, res) => {
-    res.send("Hello World BKL!!");
-});*/
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+//For Deployment
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); //for deployment purposes. express.static() --> bundles all frontend files into dist as static files.
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html")); //This script makes sure that both the backend & frontend run on only one server, i.e, localhost:5000 ---> all routes api & app are segregated automatically. And indx.html is rendered on all routes ("*"), except the api endpoints routes. 
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
