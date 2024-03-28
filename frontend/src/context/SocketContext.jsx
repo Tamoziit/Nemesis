6856b8b112613544daaa9b82978f3aca92847940
Tamojit Das <tamojitdas181007@gmail.com>
2024-03-28 21:25:32 +0530

    Deployment update 2

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://nemesis-owrb.onrender.com", {
                 query: {
                     userId: authUser._id //passiing auth user id as socket id
                 },
