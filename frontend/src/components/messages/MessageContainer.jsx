e7012a4b9d836c82c287e169c7081b9dc840bc5d
Tamojit Das <tamojitdas181007@gmail.com>
2024-04-16 21:23:54 +0530

    Responsive functionality

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -4,15 +4,20 @@ import useConversation from "../../zustand/useConversation";
 import { TiMessage } from "react-icons/ti";
 import { useEffect } from "react";
 import { useAuthContext } from "../../context/AuthContext";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const MessageContainer = () => {
-    const {selectedConversation, setSelectedConversation} = useConversation();
+    const { selectedConversation, setSelectedConversation } = useConversation();
 
     //Reseting last selected convo status on logoout
     useEffect(() => {
         //cleanup function (unmounting)
         return () => setSelectedConversation(null);
     }, [setSelectedConversation]);
+
+    const handleClick = () => {
+        setSelectedConversation(null);
+    }
 
     return (
         <div className="md:min-w-[450px] flex flex-col">
@@ -21,9 +26,12 @@ const MessageContainer = () => {
             ) : (
                 <>
                     {/* Header */}
-                    <div className="bg-slate-500 px-4 py-2 mb-2">
-                        <span className="label-text">To:{" "}{" "}</span>
-                        <span className="text-gray-900 font-bold">{selectedConversation.username}</span>
+                    <div className="bg-slate-500 px-4 py-2 mb-2 flex justify-between">
+                        <div>
+                            <span className="label-text">To:{" "}{" "}</span>
+                            <span className="text-gray-900 font-bold">{selectedConversation.username}</span>
+                        </div>
+                        <button className="text-gray-900 font-bold" onClick={handleClick}><IoMdArrowRoundBack /></button>
                     </div>
                     <Messages />
                     <MessageInput />
@@ -38,7 +46,7 @@ export default MessageContainer
 
 
 const NoChatSelected = () => {
-    const {authUser} = useAuthContext();
+    const { authUser } = useAuthContext();
 
     return (
         <div className="flex items-center justify-center w-full h-full">
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,20 @@
-import Sidebar from '../../components/sidebar/Sidebar'
-import MessageContainer from '../../components/messages/MessageContainer'
+import Sidebar from '../../components/sidebar/Sidebar';
+import MessageContainer from '../../components/messages/MessageContainer';
+import useConversation from '../../zustand/useConversation';
 
 const Home = () => {
+  const { selectedConversation } = useConversation();
+  const isSmallScreen = window.innerWidth <= 650;
+
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-        <Sidebar />
-        <MessageContainer />
+      {isSmallScreen ? (selectedConversation ? <MessageContainer /> : <Sidebar />) :
+        <>
+          <Sidebar />
+          <MessageContainer />
+        </>}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
